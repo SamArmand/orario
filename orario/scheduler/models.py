4249d70d1fd9a2a49f98e5e8a5dd3f19b446f475
Alain <i.am@soaa.me>
2014-02-25 15:27:50 -0500

    Preliminary work on models

--- orario/scheduler/models.py
+++ orario/scheduler/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+# As used by Concordia's schedules
+DAYS_OF_THE_WEEK = (
+        ('D', _('sunday')),
+        ('M', _('monday')),
+        ('T', _('tuesday')),
+        ('W', _('wednesday')),
+        ('J', _('thursday')),
+        ('F', _('friday')),
+        ('S', _('saturday')),
+)
+
+class TimeSlot(models.Model):
+    label = models.CharField(max_length=50)
+    begin_time = models.TimeField()
+    end_time = models.TimeField()
+    day = models.CharField(max_length=1, choices=DAYS_OF_THE_WEEK)
+    
+    def conflicts_with(self, slot):
+        if ((self.day == slot.day) and ((self.begin_time < slot.end_time) or (slot.begin_time < self.end_time))):
+            return true
+        return false
+    
+    class Meta:
+        abstract = True
+
+class BusySlot(TimeSlot):
+    user = models.ForeignKey(User)
+    
+class Course(models.Model):
+    number = models.CharField(max_length=10)
+    title = models.CharField(max_length=255)
+    
