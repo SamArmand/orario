360e0356df6a2e8ba8431ed49903b74231e0fb3f
GeorgeValergas <gvalergas@gmail.com>
2014-03-01 19:42:20 -0500

    add comment stubs to methods

--- orario/scheduler/models.py
+++ orario/scheduler/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
+
+"""GEORGE NOTES
+    delete this shizz
+    -- Add Pre/Post/Invariant Everywhere
+    -- Use docstrings under declaration
+    -- TimeField():
+        Time Objects
+"""
+
 # As used by Concordia's schedules
 DAYS_OF_THE_WEEK = (
         ('D', _('sunday')),
@@ -13,6 +22,8 @@ DAYS_OF_THE_WEEK = (
         ('S', _('saturday')),
 )
 
+
+
 class TimeSlot(models.Model):
     label = models.CharField(max_length=50)
     begin_time = models.TimeField()
@@ -20,17 +31,33 @@ class TimeSlot(models.Model):
     day = models.CharField(max_length=1, choices=DAYS_OF_THE_WEEK)
     
     def conflicts_with(self, slot):
-        if ((self.day == slot.day)
+        """ 
+             
+            - Precondition(s): slot must be a valid TimeSlot object => isinstance(slot,TimeSlot)
+            - Postcondition(s):none, (does not change state)
+        """
+        if ((self.day == slot.day)  
                 and ((self.begin_time < slot.end_time) 
                      or (slot.begin_time < self.end_time))):
             return True
         return False
-    
+
+    """ 
+    Modifies the outer class to configure the model for django.Used to trun a class abstract
+
+    """
     class Meta:
         abstract = True
 
+
 class BusySlot(TimeSlot):
+    """
+        - Precondition(s):
+        - Postcondition(s):
+    """
     user = models.ForeignKey(User)
+
+
     
 class Course(models.Model):
     number = models.CharField(max_length=10)
