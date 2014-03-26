6bfd21ee1a25af172598f6f788ca2cce40ea1324
Alain Trinh <i.am@soaa.me>
2014-03-25 20:00:01 -0400

    Much document. Very comment. So scheduler.models

--- orario/course_calendar/models.py
+++ orario/course_calendar/models.py
@@ -6,40 +6,102 @@ from scheduler.models import LectureSlot, TutorialSlot, LabSlot
 
 class Course(models.Model):
     """
-        Represents A Course, which is composed of one or many sections.
-
+    Represents A Course, which is composed of one or many sections.
     """
-    number = models.CharField(max_length=10)  # represented as ID in domain model
-    title = models.CharField(max_length=255)  # represented as Name in domain model
-    credits = models.DecimalField(max_digits=2, decimal_places=1)  # example: 3.5
+    #: Course number, e.g. "SOEN 331."
+    number = models.CharField(max_length=10)
+    #: A short description of the course.
+    title = models.CharField(max_length=255)
+    #: The number of credits the course is worth.
+    credits = models.DecimalField(max_digits=2, decimal_places=1)
+    #: The prerequisite courses for this course.
     prereqs = models.ManyToManyField("self", symmetrical=False, related_name='prereq_by')
+    #: The corequisite courses for this course.
     coreqs = models.ManyToManyField("self", symmetrical=False, related_name='coreq_by')
 
     def __unicode__(self):
-        return self.number
+        """Returns string representation of the course."""
+        return "%s - %s" % (self.number, self.title)
+
+    def is_coreq_for(self, course):
+        """
+        Returns whether this course is a corequisite for the specified
+        course.
+        """
+        return self in course.coreqs.all()
 
 
 class Section(models.Model):
     """
-
+    Describes a course section that is a unique combination of a lecture,
+    an optional tutorial, and an optional lab.
     """
+    #: The course this section belongs to.
     course = models.ForeignKey(Course)
+    #: The lecture in this section.
     lecture = models.ForeignKey(LectureSlot)
+    #: The tutorial in this section. Optional.
     tutorial = models.ForeignKey(TutorialSlot, null=True, blank=True)
+    #: The lab in this section. Optional.
     lab = models.ForeignKey(LabSlot, null=True, blank=True)
 
     def __unicode__(self):
+        """
+        Returns the complete section code, e.g. AA AJ AL
+        """
         return string.join([
             self.course.__unicode__(),
             self.lecture.section_code,
             self.tutorial.section_code if self.tutorial is not None else '',
             self.lab.section_code if self.lab is not None else ''])
 
+    def conflicts_with(self, other):
+        """
+        Returns true if the section has a time conflict with the specified
+        Section or TimeSlot, and false otherwise.
+        """
+        from scheduler.models import TimeSlot
+        assert isinstance(other, TimeSlot) or isinstance(other, Section)
+        if isinstance(other, TimeSlot):
+            return (
+                other.conflicts_with(self.lecture) or
+                other.conflicts_with(self.tutorial) or
+                other.conflicts_with(self.lab)
+            )
+        elif isinstance(other, Section):
+            lect_conflict = (
+                other.lecture.conflicts_with(self.lecture) or
+                other.lecture.conflicts_with(self.tutorial) or
+                other.lecture.conflicts_with(self.lab)
+            )
+            if other.tutorial is not None:
+                tut_conflict = (
+                    other.tutorial.conflicts_with(self.lecture) or
+                    other.tutorial.conflicts_with(self.tutorial) or
+                    other.tutorial.conflicts_with(self.lab)
+                )
+            else:
+                tut_conflict = False
+            if other.lab is not None:
+                lab_conflict = (
+                    other.lab.conflicts_with(self.lecture) or
+                    other.lab.conflicts_with(self.tutorial) or
+                    other.lab.conflicts_with(self.lab)
+                )
+            else:
+                lab_conflict = False
+            return (
+                lect_conflict or
+                tut_conflict or
+                lab_conflict
+            )
+
 
 class Program(models.Model):
     """
     A program can have one of many offered sequences.
     """
+    #: Name of the program.
     name = models.CharField(max_length=255)
 
 
@@ -47,6 +109,9 @@ class Option(models.Model):
     """
     An option carries a course sequence.
     """
+    #: Name of the option.
     name = models.CharField(max_length=255)
+    #: Program that the Option belongs to.
     program = models.ForeignKey(Program)
+    #: The courses listed in suggested order of completion.
     sequence = SortedManyToManyField(Course)
--- orario/scheduler/models.py
+++ orario/scheduler/models.py
@@ -25,18 +25,26 @@ SUNDAY = 0b1000000
 
 
 def validate_days(days):
+    """Validator to check if the days int is within range."""
+    assert isinstance(days, int)
     if days < 0 or days > 0b1111111:
         raise ValidationError('days out of range')
 
 
 class TimeSlot(models.Model):
+    """
+    Abstract class representing a time slot in a schedule.
+    """
+    #: A generic label that can be used by child classes.
     label = models.CharField(max_length=50)
+    #: The starting time of the itme slot.
     begin_time = models.TimeField()
+    #: The ending time of the time slot.
     end_time = models.TimeField()
+    #: Bitwise representation of the days of the week where the time slot exists.
     days = models.IntegerField(validators=[validate_days])
 
     class Meta:
-
         """
         [Django Metadata Options Class](https://docs.djangoproject.com/en/dev/topics/db/models/#meta-options)
         Modifies the outer class to configure the model for django. Used to make a class abstract
@@ -45,13 +53,11 @@ class TimeSlot(models.Model):
 
     def conflicts_with(self, slot):
         """
-        @param slot: TimeSlot object
-        @return: Boolean value, True if TimeField values intersect.
-        - Description: Compares self with parameter TimeSlot object.
-            Checks if passed parameter occupies the same space as self.
-        - Precondition(s): slot must be a valid TimeSlot object => isinstance(slot,TimeSlot)
-        - Postcondition(s): none, (does not change state)
+        Returns whether this TimeSlot conflicts with the specified
+        slot.
         """
+        if slot is None:
+            return False
         assert isinstance(slot, TimeSlot)
         if ((self.days & slot.days)  # Bitwise AND to check for conflicting days
             and ((self.begin_time < slot.end_time)
@@ -62,36 +68,48 @@ class TimeSlot(models.Model):
 
 class BusySlot(TimeSlot):
     """
-    Description: BusySlot objects represent periods of time that a user prefers not to allot to school.
+    A specialized class representing periods of time that a user prefers not
+    to allot to school.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    #: The student the BusySlot belongs to.
+    student = models.ForeignKey(settings.AUTH_USER_MODEL)
 
 
 class SectionSlot(TimeSlot):
     """
-    Contains a Course Object. Has a type of lec, tut, or lab.
-    Example: type:lec, code: 'AA', instructor: 'Aiman Hanna',
+    An abstract class that represents a class section.
     """
-    section_code = models.CharField(max_length=2)  # "Lect AA" >> Just the AA part
-    instructor = models.CharField(max_length=255)  # "Aiman Hanna"
-    room = models.CharField(max_length=255)  # "SGW H-530"
+    #: The code for the section, e.g. "AA"
+    section_code = models.CharField(max_length=2)
+    #: The instructor for the section.
+    instructor = models.CharField(max_length=255)
+    #: The room number for the section.
+    room = models.CharField(max_length=255)
 
     class Meta:
         abstract = True
 
     def __unicode__(self):
+        """Returns the section as a string, with basic info."""
         return "%s: %s, (%s-%s)" % (self.__class__.__name__,
                                     self.section_code,
                                     self.begin_time.strftime('%H:%M'),
                                     self.end_time.strftime('%H:%M'))
 
     def __init__(self, *args, **kwargs):
+        """
+        Custom constructor that can construct an object with the
+        specified dictionary.
+        """
         sect_dict = kwargs.pop('sect_dict', None)
         super(SectionSlot, self).__init__(*args, **kwargs)
         if sect_dict is not None:
             self.fill_from_dict(sect_dict)
 
     def fill_from_dict(self, sect_dict):
+        """
+        Fills the section with values from the specified dictionary.
+        """
         self.begin_time = sect_dict['begin_time']
         self.end_time = sect_dict['end_time']
         self.days = sect_dict['days']
@@ -102,18 +120,60 @@ class SectionSlot(TimeSlot):
 
 
 class LectureSlot(SectionSlot):
+    """
+    A specialized SectionSlot that represents a lecture.
+    """
     pass
 
 
 class TutorialSlot(SectionSlot):
+    """
+    A specialized SectionSlot that represents a tutorial.
+    """
     pass
 
 
 class LabSlot(SectionSlot):
+    """
+    A specialized SectionSlot that represents a lab.
+    """
     pass
 
 
 class Schedule(models.Model):
     """
-    Contains Timeslot objects.
+    Holds schedule information for a single school term.
+    Responsible for populating itself given set constraints.
     """
+    #: The student who owns the schedule.
+    student = models.ForeignKey(settings.AUTH_USER_MODEL)
+    #: The term number as per Concordia University's conventions.
+    term = models.IntegerField()
+    #: Auto-generated schedule must contain these courses.
+    courses = models.ManyToManyField('Course')
+    #: Auto-generated schedule must contain these sections.
+    sections = models.ManyToManyField('Section')
+
+    def add_course(self, course):
+        """
+        Adds course into schedule if all prereqs and coreqs are met.
+        Prereqs are either in student's record (student.courses_taken)
+        or in a preceding schedule.
+        """
+        # TODO: Figure out how to check preceding schedules
+        from course_calendar.models import Course
+        assert isinstance(course, Course)
+        if (
+            all(prereq in self.courses.all() for prereq in self.student.courses_taken.all())
+            and
+            all(coreq in self.courses.all() for coreq in course.coreqs.all())
+        ):
+            self.courses.add(course)
+            return True
+        else:
+            return False
+
+    def remove_course(self, course):
+        from course_calendar.models import Course
+        assert isinstance(course, Course)
+        self.courses.filter(coreqs__contains=course).remove()
