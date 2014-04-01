874cf3807aedf8000ca2efd9f90b39067c09552f
Alain Trinh <i.am@soaa.me>
2014-04-01 16:58:33 -0400

    Formatting

--- orario/scheduler/tests.py
+++ orario/scheduler/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
 from models import *
 from student.models import *
 from course_calendar.models import *
 
 from datetime import time
+
 
 class ValidateDaysTestCase(TestCase):
     def test_validate_days_success(self):
@@ -31,10 +31,12 @@ class ValidateDaysTestCase(TestCase):
             ex = True
         self.assertTrue(ex)
 
+
 class TimeSlotTestCase(TestCase):
     """
     Run these tests by running manage.py test
     """
+
     def setUp(self):
         """
         Inserts dummy data into the database for testing.
@@ -43,7 +45,7 @@ class TimeSlotTestCase(TestCase):
             label="",
             begin_time=time(8, 45),
             end_time=time(10, 00),
-            days=WEDNESDAY|FRIDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=WEDNESDAY | FRIDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="AA",
             instructor="Gangnam Style",
             room="SGW H820"
@@ -53,7 +55,7 @@ class TimeSlotTestCase(TestCase):
             label="",
             begin_time=time(9, 45),
             end_time=time(11, 00),
-            days=TUESDAY|FRIDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=TUESDAY | FRIDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="BB",
             instructor="PSY",
             room="SGW H820"
@@ -63,7 +65,7 @@ class TimeSlotTestCase(TestCase):
             label="",
             begin_time=time(9, 45),
             end_time=time(11, 00),
-            days=TUESDAY|THURSDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=TUESDAY | THURSDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="CC",
             instructor="PSY",
             room="SGW H820"
@@ -73,7 +75,7 @@ class TimeSlotTestCase(TestCase):
             label="",
             begin_time=time(10, 15),
             end_time=time(11, 30),
-            days=WEDNESDAY|FRIDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=WEDNESDAY | FRIDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="DD",
             instructor="PSY",
             room="SGW H820"
@@ -132,7 +134,7 @@ class ScheduleTestCase(TestCase):
             label="",
             begin_time=time(8, 45),
             end_time=time(10, 00),
-            days=WEDNESDAY|FRIDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=WEDNESDAY | FRIDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="AA",
             instructor="Gangnam Style",
             room="SGW H820"
@@ -141,7 +143,7 @@ class ScheduleTestCase(TestCase):
             label="",
             begin_time=time(9, 45),
             end_time=time(11, 00),
-            days=TUESDAY|FRIDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=TUESDAY | FRIDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="BB",
             instructor="PSY",
             room="SGW H820"
@@ -150,7 +152,7 @@ class ScheduleTestCase(TestCase):
             label="",
             begin_time=time(9, 45),
             end_time=time(11, 00),
-            days=TUESDAY|THURSDAY,  # Bitwise or to set both wed and fri bits to 1
+            days=TUESDAY | THURSDAY,  # Bitwise or to set both wed and fri bits to 1
             section_code="CC",
             instructor="PSY",
             room="SGW H820"
